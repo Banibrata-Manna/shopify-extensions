c345d0ff1d24a8f1edaf163d3d76aa672508f96c
Banibrata Manna <banibratamanna@commons-MacBook-Air.local>
2025-08-11 12:19:12 +0530

    Pickup Button and Store blocks

---
+++ extensions/hc-dev-theme-extension/assets/hcBopis.js
@@ -0,0 +1,19 @@
+export function getStores() {
+    return [
+        {
+            name: "Demo Store",
+            address: "Demo Address",
+            timings: "10 AM - 9 PM"
+        },
+        {
+            name: "Another Store",
+            address: "Another Address",
+            timings: "9 AM - 8 PM"
+        },
+        {
+            name: "Third Store",
+            address: "Third Address",
+            timings: "11 AM - 7 PM"
+        }
+    ];
+}
