74b1656372cf6d175fde22763f1dddf0f2d365d9
Banibrata Manna <banibratamanna@commons-MacBook-Air.local>
2025-08-11 19:29:47 +0530

    Used DOM Manipulation to populate stores in Store List Snippet

--- extensions/hc-dev-theme-extension/assets/hcBopis.js
+++ extensions/hc-dev-theme-extension/assets/hcBopis.js
@@ -1,19 +1,40 @@
-export function getStores() {
+function getStores() {
     return [
-        {
-            name: "Demo Store",
-            address: "Demo Address",
-            timings: "10 AM - 9 PM"
-        },
-        {
-            name: "Another Store",
-            address: "Another Address",
-            timings: "9 AM - 8 PM"
-        },
-        {
-            name: "Third Store",
-            address: "Third Address",
-            timings: "11 AM - 7 PM"
-        }
+        { name: "Demo Store", address: "Demo Address", timings: "10 AM - 9 PM", "city": "Aventura",
+                "postalCode": "33180",
+                "country": "United States",
+                "countryCode": "US",
+                "state": "Florida"},
+        { name: "Another Store", address: "Another Address", timings: "9 AM - 8 PM", "city": "Miami",
+                "postalCode": "33101",
+                "country": "United States",
+                "countryCode": "US",
+                "state": "Florida"},
+        { name: "Third Store", address: "Third Address", timings: "11 AM - 7 PM", "city": "Orlando",
+                "postalCode": "32801",
+                "country": "United States",
+                "countryCode": "US",
+                "state": "Florida"}
     ];
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var stores = getStores();
+    var container = document.getElementById('store-list');
+    if (container) {
+        stores.forEach(function(store) {
+            var html = 
+            `<div class="store">
+                <h3 class="store-name">${store.name}</h3>
+                <p>${store.address}</p>
+                <p>Open Today: ${store.timings}</p>
+            </div>
+            <div class="store-actions">
+                <button id="pickup-btn" class="btn">Pickup Here</button>
+                <p>In Stock</p>
+            </div>
+            <hr class="custom-line">`;
+            container.insertAdjacentHTML('beforeend', html);
+        });
+    }
+});
