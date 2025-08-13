d748816e4c32c97215213497f679ed8763c52dff
Banibrata Manna <banibratamanna@commons-MacBook-Air.local>
2025-08-14 01:09:51 +0530

    Made Similar UI for inline and modal store list and getting stores from oms

--- extensions/hc-dev-theme-extension/assets/hcBopis.js
+++ extensions/hc-dev-theme-extension/assets/hcBopis.js
@@ -1,58 +1,76 @@
-function getStores() {
-    return [
-        {
-            name: "Demo Store", address: "Demo Address", timings: "10 AM - 9 PM", "city": "Aventura",
-            "postalCode": "33180",
-            "country": "United States",
-            "countryCode": "US",
-            "state": "Florida"
-        },
-        {
-            name: "Another Store", address: "Another Address", timings: "9 AM - 8 PM", "city": "Miami",
-            "postalCode": "33101",
-            "country": "United States",
-            "countryCode": "US",
-            "state": "Florida"
-        },
-        {
-            name: "Third Store", address: "Third Address", timings: "11 AM - 7 PM", "city": "Orlando",
-            "postalCode": "32801",
-            "country": "United States",
-            "countryCode": "US",
-            "state": "Florida"
-        }
-    ];
+// TODO: Implement Searching Stores by lat, lon & zipcode.
+async function getStores() {
+  let stores = [];
+
+  await fetch('<storeLookup url of oms instance>', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        viewSize: 10,
+        filters: ["pickup_pref: true", "storeType: RETAIL_STORE"],
+        sortBy: "storeName asc",
+        fieldsToSelect: "storeName, , address1, countryCode, postalCode, city"
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      stores = data?.response?.docs;
+    })
+    .catch(error => {
+      console.error('Error fetching stores:', error);
+    });
+    return stores;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var stores = getStores();
+async function generateStoreListHTML(container, enablePickup) {
+  const storeActions = enablePickup
+      ? `<div class="store-actions">
+           <button id="pickup-btn" class="btn">Pickup Here</button>
+         </div>`
+      : '';
+
+  const stores = await getStores();
+  stores.forEach(store => {
+  const storeName = store.storeName || '';
+  const address1 = store.address1 || '';
+  const city = store.city || '';
+  const postalCode = store.postalCode || '';
+  const countryCode = store.countryCode || '';
+  const phone = store.phone || '';
+  const timings = store.timings || '';
+
+  const html = `
+    <div class="store">
+      <div class="store-details">
+        ${storeName ? `<h3 class="store-name">${storeName}</h3>` : ''}
+        ${address1 ? `<p>${address1}</p>` : ''}
+        ${(city || postalCode || countryCode) ? `<p>${[city, postalCode, countryCode].filter(Boolean).join(', ')}</p>` : ''}
+      </div>
+      <div class="store-inv-contacts">
+        <p>In Stock</p>
+        ${phone ? `<p>Phone: ${phone}</p>` : ''}
+        ${timings ? `<p>Open Today: ${timings}</p>` : ''}
+      </div>
+    </div>
+    ${storeActions}
+    <hr class="custom-line">
+  `;
+
+  container.insertAdjacentHTML('beforeend', html);
+});
+}
+
+document.addEventListener('DOMContentLoaded', async function () {    
     var container = document.getElementById('store-list');
     if (container) {
-        var showPickupHere = container.dataset.showPickupHere
-        stores.forEach(function (store) {
-            const storeActions = showPickupHere === "true"
-                ? `<div class="store-actions">
-                <button id="pickup-btn" class="btn">Pickup Here</button>
-                <p>In Stock</p>
-            </div>` : '';
-
-            const html = `
-                <div class="store">
-                    <h3 class="store-name">${store.name}</h3>
-                    <p>${store.address}</p>
-                    <p>Open Today: ${store.timings}</p>
-                </div>
-                ${storeActions}
-                <hr class="custom-line">
-            `;
-
-            container.insertAdjacentHTML('beforeend', html);
-
-        });
+      var showPickupHere = container.dataset.showPickupHere;
+      await generateStoreListHTML(container, showPickupHere === "true");
     }
 });
 
-function showPickupModal(enablePickup) {
+async function showPickupModal(enablePickup) {
   const modal = document.getElementById('pickup-modal-embed');
   if (!modal) return;
 
@@ -62,27 +80,8 @@ function showPickupModal(enablePickup) {
   if (!container) return;
 
   container.innerHTML = '';
-  
-  const stores = getStores();
-  stores.forEach(store => {
-    const storeActions = enablePickup
-      ? `<div class="store-actions">
-           <button id="pickup-btn" class="btn">Pickup Here</button>
-           <p>In Stock</p>
-         </div>`
-      : '';
 
-    const html = `
-      <div class="store">
-        <h3 class="store-name">${store.name}</h3>
-        <p>${store.address}</p>
-        <p>Open Today: ${store.timings}</p>
-      </div>
-      ${storeActions}
-      <hr class="custom-line">
-    `;
-    container.insertAdjacentHTML('beforeend', html.trim());
-  });
+  await generateStoreListHTML(container, enablePickup);
 }
 
 function closePickupModal() {
